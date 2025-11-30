66a74fd3c8dda6cdd6b6be2f1fabe46cbd9c6872
ahmadmuqri0 <muqrisuhaimi25@gmail.com>
2025-11-30 08:15:01 +0800

    Add python

--- common/.config/nvim/lua/config/lazy.lua
+++ common/.config/nvim/lua/config/lazy.lua
@@ -32,6 +32,7 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     { import = "lazyvim.plugins.extras.lang.svelte" },
     { import = "lazyvim.plugins.extras.lang.vue" },
+    { import = "lazyvim.plugins.extras.lang.python" },
     { import = "lazyvim.plugins.extras.lang.typescript" },
     { import = "lazyvim.plugins.extras.lang.java" },
     { import = "lazyvim.plugins.extras.lang.json" },
